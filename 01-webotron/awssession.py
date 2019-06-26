52166cad201fcd578af947d5cdb6169ab64c4997
Devin Ybarra <devinybarra@Devins-MBP.satx.rr.com>
2019-06-25 22:12:53 -0500

    setup project directory for webotron.

---
+++ 01-webotron/awssession.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import boto3
+session = boto3.Session
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket)
+    
+new_bucket = s3.create_bucket(Bucket='devin-bucket-3')
+for bucket in s3.buckets.all():
+    print(bucket)
+    
+get_ipython().run_line_magic('history', '')
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # automating-aws-with-python by devin ybarra
+
+## 01-webotron
+
+Webotron is a script that will sync a local directory to an s3 bucket, configure Route 53, and cloudfront.
